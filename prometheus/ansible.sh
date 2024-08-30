407c431abe5015bfc34860f9e38243476fbc6a22
akr9757 <anilkumareddy9757@gmail.com>
2024-08-30 13:13:49 +0530

    prometheus code

--- prometheus/ansible.sh
+++ prometheus/ansible.sh
@@ -1,4 +1,7 @@
-ssh_username=${aws ssm get-parameter  --name "ssh.username" --with-decryption --query "parameter.value" --output text}
-ssh_password=${aws ssm get-parameter  --name "ssh.password" --with-decryption --query "parameter.value" --output text}
+#ssh_username=${aws ssm get-parameter  --name "ssh.username" --with-decryption --query "parameter.value" --output text}
+#ssh_password=${aws ssm get-parameter  --name "ssh.password" --with-decryption --query "parameter.value" --output text}
+
+ssh_username=${aws ssm get-parameter --name "ssh.username" --query "Parameter.Value" --output text}
+ssh_password=${aws ssm get-parameter --name "ssh.password" --query "Parameter.Value" --output text}
 
 ansible-playbook -i prometheus-pvt.akrdevops.online, prometheus.yml -e ansible_user=${ssh_username} -e ansible_password=$ssh_password
